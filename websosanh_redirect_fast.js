3ee0cf61d5c431ef2326cb90a8bcefeb63014f65
thongdong7 <thongdong7@users.noreply.github.com>
2019-04-04 06:27:12 +0700

    Update websosanh_redirect_fast.js

--- websosanh_redirect_fast.js
+++ websosanh_redirect_fast.js
@@ -1,22 +1,41 @@
 // ==UserScript==
 // @name         Websosanh
-// @copyright    Internet
-// @license      MIT
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        http://websosanh.vn/*/*/*/direct.htm
+// @match        https://websosanh.vn/*/*/*/direct.htm
 // @grant        none
 // ==/UserScript==
 
-// ==OpenUserJS==
-// @author       You
-// ==/OpenUserJS==
 (function() {
     'use strict';
 
     // Your code here...
+    window.pageOptions={}
     const form = document.getElementsByTagName("form")[0];
-    form.submit();
+    console.log(form);
+
+    const action = form.getAttribute("action");
+    //url=https%3A%2F%2Ftiki.vn%2Fvoi-hoa-sen-tam-dung-cao-cap-ye15-p6093963.html%3F
+    //form.parentNode.removeChild(form);
+
+    var regex = /url=(.*)(%3F)?/;
+    console.log('action', action);
+    var found = action.match(regex);
+    console.log('found', found);
+    let url;
+    if (found) {
+        url = found[1];
+    }
+    console.log('url', url);
+    if (url != null && url != '') {
+        url = unescape(url);
+        console.log('url2', url);
+        window.location = url;
+    } else {
+        console.log('submit');
+        form.submit();
+    }
 })();
