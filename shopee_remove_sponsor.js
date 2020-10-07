a59293ba5752bc5cf37cad15fb5aedb8f9847a1a
thongdong7 <thongdong7@users.noreply.github.com>
2020-10-07 18:04:39 +0700

    Update shopee_remove_sponsor.js

--- shopee_remove_sponsor.js
+++ shopee_remove_sponsor.js
@@ -11,11 +11,29 @@
 (function() {
     'use strict';
 
+    function remove() {
+        const items = [...document.getElementsByTagName("div")]
+            .filter(item => item.innerText.trim().startsWith('Tài Trợ'));
+
+        //    .map(item => item)
+        //for (let i=0;
+        //const item = items[items.length - 1];
+        for (const item of items){
+            const node = item.parentNode.parentNode;
+            if (node ==null || node.tagName.toLowerCase() !== 'div') {
+                continue;
+            }
+
+            if (!node.getAttribute('class')?.startsWith('_')) {
+                continue;
+            }
+            console.log(node);
+            node.parentNode.removeChild(node);
+            //            .forEach(item => item.parentNode.parentNode.removeChild(item.parentNode))
+        }
+    }
     // Your code here...
     setInterval(() => {
-        [...document.getElementsByTagName("div")]
-            .filter(item => item.innerText.trim().startsWith('Tài Trợ'))
-            .map(item => item)
-            .forEach(item => item.parentNode.removeChild(item))
+        remove();
     }, 1000);
 })();
